faba8e6da16133dfce1474b6581a489de4fefa00
Jay Dave <jaycdave@gmail.com>
2015-04-03 09:34:45 -0700

    removed added period in devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'jaycdave@gmail.com'
-  config.mailer = "LoanMailer".
+  config.mailer = "LoanMailer"
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
