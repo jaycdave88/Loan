59ef9815c0909e58a0cc6e6ac293d38b89f2e883
Jay Dave <jaycdave@gmail.com>
2015-04-03 10:03:04 -0700

    added s3 config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
@@ -76,4 +76,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['roostify'],
+      :access_key_id => ENV['AKIAIQQ537J2OALVPNAA
+        '],
+        :secret_access_key => ENV['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
+          ']
+        }
+      }
+    end
